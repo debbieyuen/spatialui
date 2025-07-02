cc36dca4fc188c720fb80fb49e2e28b35657af02
Deborah Yuen <deborahyuen@berkeley.edu>
2025-07-02 16:24:33 -0700

    initial visionOS app

---
+++ squiggly/squiggly/squigglyApp.swift
@@ -0,0 +1,17 @@
+//
+//  squigglyApp.swift
+//  squiggly
+//
+//  Created by Debbie Yuen on 7/2/25.
+//
+
+import SwiftUI
+
+@main
+struct squigglyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ squiggly/squigglyTests/squigglyTests.swift
@@ -0,0 +1,17 @@
+//
+//  squigglyTests.swift
+//  squigglyTests
+//
+//  Created by Debbie Yuen on 7/2/25.
+//
+
+import Testing
+@testable import squiggly
+
+struct squigglyTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
