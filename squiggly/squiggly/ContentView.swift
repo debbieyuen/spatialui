3a94c176647f4f132e4a3ea28f49d52746cc9146
Deborah Yuen <deborahyuen2016@berkeley.edu>
2025-07-06 17:28:50 -0700

    incldue ARKit files and packages

--- squiggly/squiggly/ContentView.swift
+++ squiggly/squiggly/ContentView.swift
@@ -3,6 +3,7 @@
 //  squiggly
 //
 //  Created by Debbie Yuen on 7/2/25.
+//  ARKit: https://developer.apple.com/documentation/visionOS/setting-up-access-to-arkit-data
 //
 
 import SwiftUI
@@ -10,12 +11,21 @@ import RealityKit
 import RealityKitContent
 
 struct ContentView: View {
+    // Call openImmersive Space to create a space to run ARKit session
+    @Environment(\.openImmersiveSpace) private var openImmersiveSpace
+    
     var body: some View {
         VStack {
             Model3D(named: "Scene", bundle: realityKitContentBundle)
                 .padding(.bottom, 80)
 
             Text("Hello, world!")
+            
+            Button("Start ARKit experience") {
+                Task {
+                    await openImmersiveSpace(id: "appSpace")
+                }
+            }
         }
         .padding()
     }
