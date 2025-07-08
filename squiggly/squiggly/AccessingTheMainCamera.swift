00ff4d62a44e6e22a26956d66568b95a23132986
Deborah Yuen <deborahyuen2016@berkeley.edu>
2025-07-07 17:59:01 -0700

    access the main camera visionos

---
+++ squiggly/squiggly/AccessingTheMainCamera.swift
@@ -0,0 +1,31 @@
+//
+//  AccessingTheMainCamera.swift
+//  squiggly
+//
+//  Created by Debbie Yuen on 7/7/25.
+//
+
+import SwiftUI
+
+struct AccessingTheMainCameraApp: App {
+    @State private var appModel = AppModel()
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+                .environment(appModel)
+        }
+
+        ImmersiveSpace(id: appModel.immersiveSpaceID) {
+            ImmersiveView()
+                .environment(appModel)
+                .onAppear {
+                    appModel.immersiveSpaceState = .open
+                }
+                .onDisappear {
+                    appModel.immersiveSpaceState = .closed
+                }
+        }
+        .immersionStyle(selection: .constant(.mixed), in: .mixed)
+     }
+}
