ad2a95e85b7824aa1da4c35c1a92fa47f99a3653
Deborah Yuen <deborahyuen2016@berkeley.edu>
2025-07-11 20:25:38 -0700

    use nav view and nav links instead

--- squiggly/squiggly/ContentView.swift
+++ squiggly/squiggly/ContentView.swift
@@ -15,63 +15,62 @@ struct ContentView: View {
     @Environment(\.openImmersiveSpace) private var openImmersiveSpace
     // Grab image file for the Vision Framework
     @State private var imageFile: ImageFile? = nil
-    // Variable for showing new or next view
-    @State private var showModelView = false
 
     var body: some View {
-        ZStack(alignment: .topLeading) {
-            // Header
-            HStack {
-                // Left Header
+        NavigationStack {
+            ZStack(alignment: .topLeading) {
+                // Header
                 HStack {
-                    Image("yourLogoAssetName")
-                        .resizable()
-                        .frame(width: 40, height: 40)
-                        .clipShape(Circle())
+                    // Left Header
+                    HStack {
+                        Image("yourLogoAssetName")
+                            .resizable()
+                            .frame(width: 40, height: 40)
+                            .clipShape(Circle())
 
-                    Text("Squiggly App")
-                        .font(.headline)
-                        .foregroundColor(.white)
-                }
+                        Text("Squiggly App")
+                            .font(.headline)
+                            .foregroundColor(.white)
+                    }
 
-                Spacer()
+                    Spacer()
 
-                // Right Header
-                HStack {
-                    topButton(title: "Course Website", url: "https://theapplevisionpro.vercel.app")
-                    topButton(title: "GitHub Repo", url: "https://github.com/debbieyuen/spatialui")
-//                    topButton(title: "Help", url: "https://example.com/c")
-                }
-            }
-            .padding(20)
-
-            // Main Body Section
-            HStack(spacing: 0) {
-                // Left side
-                VStack(alignment: .leading) {
-                    Text("Welcome to Vancouver")
-                        .font(.system(size: 50, weight: .bold))
-                        .padding(.bottom, 15)
-                        .accessibilitySortPriority(4)
-
-                    Text("Thank you for joining our session today at SIGGRAPH Vancouver! We are excited to have you join us. Here, you will find our demo application that you may experiment with as you wish. Try modifying the code and creating your own user interfaces. Please do not hesitate to raise your hands if you have any questions.").accessibilitySortPriority(3)
-                    
-                    Button("Start") {
-                        showModelView = true
+                    // Right Header
+                    HStack {
+                        topButton(title: "Course Website", url: "https://theapplevisionpro.vercel.app")
+                        topButton(title: "GitHub Repo", url: "https://github.com/debbieyuen/spatialui")
+    //                    topButton(title: "Help", url: "https://example.com/c")
                     }
                 }
-                .frame(maxWidth: .infinity, maxHeight: .infinity, alignment: .leading)
-                .padding(40)
+                .padding(20)
 
-                // Right Side
-                VStack {
-                        Model3D(named: "Scene", bundle: realityKitContentBundle)
+                // Main Body Section
+                HStack(spacing: 0) {
+                    // Left side
+                    VStack(alignment: .leading) {
+                        Text("Welcome to Vancouver")
+                            .font(.largeTitle)
+                            .padding(.bottom, 15)
+                            .accessibilitySortPriority(4)
+
+                        Text("Thank you for joining our session today at SIGGRAPH Vancouver! We are excited to have you join us. Here, you will find our demo application that you may experiment with as you wish. Try modifying the code and creating your own user interfaces. Please do not hesitate to raise your hands if you have any questions.").accessibilitySortPriority(3)
+                        
+                        NavigationLink(destination: MachineLearningModelsView()) {
+                            Text("Start")
+                        }
+                        .padding(.top)
+                        
+                    }
+                    .frame(maxWidth: .infinity, maxHeight: .infinity, alignment: .leading)
+                    .padding(40)
+
+                    // Right Side
+                    VStack {
+                            Model3D(named: "Scene", bundle: realityKitContentBundle)
+                    }
+                    .frame(maxWidth: .infinity, maxHeight: .infinity, alignment: .center)
+                    .padding(40)
                 }
-                .frame(maxWidth: .infinity, maxHeight: .infinity, alignment: .center)
-                .padding(40)
-            }
-            .fullScreenCover(isPresented: $showModelView) {
-                MachineLearningModelsView()
             }
         }
     }
