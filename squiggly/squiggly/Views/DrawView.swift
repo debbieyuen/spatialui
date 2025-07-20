028dd8115347109da90885b17ef8e4744c58b2fb
Deborah Yuen <deborahyuen2016@berkeley.edu>
2025-07-20 13:51:01 -0700

    add color picker

---
+++ squiggly/squiggly/Views/DrawView.swift
@@ -0,0 +1,18 @@
+//
+//  DrawView.swift
+//  squiggly
+//
+//  Created by Debbie Yuen on 7/20/25.
+//
+
+import SwiftUI
+
+struct DrawView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    DrawView()
+}
