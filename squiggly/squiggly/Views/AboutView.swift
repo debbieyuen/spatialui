8ec91c32802f0cf9a8d31c7be6ff2fc9707360ae
Deborah Yuen <deborahyuen2016@berkeley.edu>
2025-07-20 18:33:32 -0700

    3d squiggly text

--- squiggly/squiggly/Views/AboutView.swift
+++ squiggly/squiggly/Views/AboutView.swift
@@ -3,6 +3,8 @@
 //  squiggly
 //
 //  Created by Debbie Yuen on 7/19/25.
+//  Resources: https://developer.apple.com/documentation/visionos/adding-a-depth-effect-to-text-in-visionos
+//  Resources: 
 //
 
 import SwiftUI
@@ -29,6 +31,22 @@ struct AboutView: View {
                 // Right Side
                 VStack {
                     RealityView { content in
+                        // 3D text setup
+                        if let textMesh = try? await MeshResource.generateText(
+                            "squiggly",
+                            extrusionDepth: 0.02,
+                            font: UIFont(name: "SnellRoundhand-Bold", size: 0.2) ?? .systemFont(ofSize: 0.2),
+                            containerFrame: .zero,
+                            alignment: .center,
+                            lineBreakMode: .byWordWrapping
+                        ) {
+                            let textMaterial = SimpleMaterial(color: .purple, isMetallic: false)
+                            let textEntity = ModelEntity(mesh: textMesh, materials: [textMaterial])
+                            textEntity.position = [-0.07, -0.1, 0.0]
+                            textEntity.scale = [0.2, 0.5, 0.5]
+                            content.add(textEntity)
+                        }
+                        // 3D Model Pixel
                         if let entity = try? await Entity(named: "PinkPixel22", in: realityKitContentBundle) {
                             entity.scale = SIMD3<Float>(repeating: 0.65)
                             entity.position.y += 0.1   // move up 0.2 meters
